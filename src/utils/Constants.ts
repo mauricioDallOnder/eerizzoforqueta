f2df19f306ae621d762be4ca7fdab0d780b3b665
eerizzoforqueta <158513004+eerizzoforqueta@users.noreply.github.com>
2024-02-02 12:30:54 -0300

    Update Constants.ts

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -231,3 +231,27 @@ export function gerarDiasDoMes(
   }
   return datas;
 }
+
+
+const resizeImage = (file: File) => {
+    return new Promise<string>((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = (event) => {
+        const img = new Image();
+        img.src = event.target?.result as string;
+        img.onload = () => {
+          const elem = document.createElement('canvas');
+          const scaleFactor = Math.min(512 / img.width, 768 / img.height);
+          elem.width = img.width * scaleFactor;
+          elem.height = img.height * scaleFactor;
+          const ctx = elem.getContext('2d');
+          ctx?.drawImage(img, 0, 0, img.width * scaleFactor, img.height * scaleFactor);
+          const url = elem.toDataURL('image/jpeg', 1);
+          resolve(url);
+        };
+      };
+      reader.onerror = error => reject(error);
+    });
+  };
+export default resizeImage
