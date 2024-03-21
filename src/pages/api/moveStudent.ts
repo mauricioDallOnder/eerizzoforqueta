605d94c09da84270c6a9ceeb1db17459eafd4fff
eerizzoforqueta <158513004+eerizzoforqueta@users.noreply.github.com>
2024-03-21 00:18:15 -0300

    Update moveStudent.ts

--- src/pages/api/moveStudent.ts
+++ src/pages/api/moveStudent.ts
@@ -1,34 +1,25 @@
-// pages/api/moveStudent.ts
 import type { NextApiRequest, NextApiResponse } from 'next';
 import admin from '../../config/firebaseAdmin';
-import { Aluno, Turma } from '@/interface/interfaces';
+import { Aluno } from '@/interface/interfaces';
 
-// Função auxiliar para atualizar capacidade da turma e contador de alunos
-async function atualizarTurma(modalidade: string, nomeTurma: string, aluno: any, incremento: number) {
-  const turmaRef = admin.database().ref(`modalidades/${modalidade}/turmas`).orderByChild('nome_da_turma').equalTo(nomeTurma);
+const db = admin.database();
+
+async function atualizarTurma(modalidade: string, nomeTurma: string, aluno: Aluno | null, incremento: number) {
+  const turmaRef = db.ref(`modalidades/${modalidade}/turmas`).orderByChild('nome_da_turma').equalTo(nomeTurma);
   const snapshot = await turmaRef.once('value');
   if (snapshot.exists()) {
     const turmaData = snapshot.val();
     const turmaKey = Object.keys(turmaData)[0];
     const turma = turmaData[turmaKey];
 
-
-     
-
-
-
-    // Se o aluno está sendo adicionado, atualize o contador e adicione o aluno
-    if (incremento > 0) {
+    if (incremento > 0 && aluno !== null) { // Adiciona aluno
       const novoIdAluno = turma.contadorAlunos ? turma.contadorAlunos + 1 : 1;
       aluno.id = novoIdAluno;
-      if (!turma.alunos) {
-        turma.alunos = {};
-      }
+      turma.alunos = turma.alunos || {};
       turma.alunos[novoIdAluno] = aluno;
-    }
+    } // Não precisa de um else aqui, pois não estamos tratando remoção nesta parte
 
-    // Atualize a turma com a nova capacidade e, se necessário, com os alunos atualizados
-    await admin.database().ref(`modalidades/${modalidade}/turmas/${turmaKey}`).update({
+    await db.ref(`modalidades/${modalidade}/turmas/${turmaKey}`).update({
       alunos: turma.alunos,
       contadorAlunos: turma.contadorAlunos ? turma.contadorAlunos + incremento : 1,
       capacidade_atual_da_turma: turma.capacidade_atual_da_turma + incremento
@@ -36,77 +27,47 @@ async function atualizarTurma(modalidade: string, nomeTurma: string, aluno: any,
   }
 }
 
-export default async function moveStudent(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
-    try {
-      const {
-        alunoNome,
-        modalidadeOrigem,
-        nomeDaTurmaOrigem,
-        modalidadeDestino,
-        nomeDaTurmaDestino,
-      } = req.body;
-
-      // Encontrar o aluno na turma de origem e obtém os dados do aluno
-      const alunoDados = await encontrarEremoverAluno(modalidadeOrigem, nomeDaTurmaOrigem, alunoNome);
-
-      // Se o aluno está sendo movido para "excluidos", remova o aluno sem adicioná-lo a uma nova turma
-     if (modalidadeDestino === "excluidos") {
-      const alunoRemovido = await encontrarEremoverAluno(modalidadeOrigem, nomeDaTurmaOrigem, alunoNome);
-      if (alunoRemovido) {
-        await atualizarTurma(modalidadeOrigem, nomeDaTurmaOrigem, null, -1);
-        return res.status(200).json({ message: 'Aluno removido com sucesso' });
-      } else {
-        return res.status(404).json({ error: 'Aluno não encontrado na turma de origem' });
-      }
-    }
-
-      // Se o aluno foi encontrado e removido, adicione à nova turma e atualize o contador
-      if (alunoDados) {
-        await atualizarTurma(modalidadeDestino, nomeDaTurmaDestino, alunoDados, 1);
-        await atualizarTurma(modalidadeOrigem, nomeDaTurmaOrigem, null, -1);
-        return res.status(200).json({ message: 'Aluno movido com sucesso' });
-      } else {
-        return res.status(404).json({ error: 'Aluno não encontrado na turma de origem' });
-      }
-    } catch (error) {
-      console.error('Erro ao mover aluno', error);
-      return res.status(500).json({ error: 'Erro ao mover aluno' });
-    }
-  } else {
-    res.setHeader('Allow', 'POST');
-    res.status(405).end('Method Not Allowed');
-  }
-}
-
-// Função auxiliar para encontrar e remover aluno da turma de origem
-async function encontrarEremoverAluno(modalidade: string, nomeDaTurma: string, alunoNome: string) {
-  const turmaRef = admin.database().ref(`modalidades/${modalidade}/turmas`).orderByChild('nome_da_turma').equalTo(nomeDaTurma);
+async function encontrarEremoverAluno(modalidade: string, nomeDaTurma: string, alunoNome: string): Promise<Aluno | null> {
+  const turmaRef = db.ref(`modalidades/${modalidade}/turmas`).orderByChild('nome_da_turma').equalTo(nomeDaTurma);
   const snapshot = await turmaRef.once('value');
   if (snapshot.exists()) {
     const turmaData = snapshot.val();
     const turmaKey = Object.keys(turmaData)[0];
     const turma = turmaData[turmaKey];
 
-    let alunoIdToRemove;
-    let alunoToRemove;
     for (const [id, alunoObject] of Object.entries(turma.alunos || {})) {
-      // Aqui usamos um type assertion para dizer ao TypeScript que `alunoObject` é do tipo `Aluno`
+      // Assumimos que alunoObject é do tipo Aluno com uma asserção de tipo
       const aluno = alunoObject as Aluno;
-      if (aluno && aluno.nome === alunoNome) {
-        alunoIdToRemove = id;
-        alunoToRemove = aluno;
-        break;
+      if (aluno.nome === alunoNome) {
+        delete turma.alunos[id];
+        await db.ref(`modalidades/${modalidade}/turmas/${turmaKey}/alunos`).set(turma.alunos || {});
+        // Agora que garantimos para o TypeScript que aluno é do tipo Aluno, podemos espalhá-lo sem erro
+        return { ...aluno, id: parseInt(id) };
       }
     }
-    
-    
-
-    if (alunoIdToRemove) {
-      delete turma.alunos[alunoIdToRemove];
-      await admin.database().ref(`modalidades/${modalidade}/turmas/${turmaKey}/alunos`).set(turma.alunos);
-      return alunoToRemove;
-    }
   }
-  return null; // Aluno não encontrado
+  return null;
+}
+
+export default async function moveStudent(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'POST') {
+    res.setHeader('Allow', 'POST');
+    return res.status(405).end('Method Not Allowed');
+  }
+
+  try {
+    const { alunoNome, modalidadeOrigem, nomeDaTurmaOrigem, modalidadeDestino, nomeDaTurmaDestino } = req.body;
+    const alunoDados = await encontrarEremoverAluno(modalidadeOrigem, nomeDaTurmaOrigem, alunoNome) as Aluno | null;
+
+    if (alunoDados) {
+      // Agora você pode usar alunoDados com segurança porque o tipo foi asserido para Aluno | null
+      await atualizarTurma(modalidadeDestino, nomeDaTurmaDestino, alunoDados, 1);
+      await atualizarTurma(modalidadeOrigem, nomeDaTurmaOrigem, null, -1);
+    } else {
+      return res.status(404).json({ error: 'Aluno não encontrado na turma de origem' });
+    }
+  } catch (error) {
+    console.error('Erro ao mover aluno', error);
+    return res.status(500).json({ error: 'Erro ao mover aluno' });
+  }
 }
