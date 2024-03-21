e902f6fbdbc852d0c4c5ace28fd2755ca461b15d
eerizzoforqueta <158513004+eerizzoforqueta@users.noreply.github.com>
2024-03-21 00:18:58 -0300

    Update SubmitFormRegistration.ts

--- src/pages/api/SubmitFormRegistration.ts
+++ src/pages/api/SubmitFormRegistration.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import admin from '../../config/firebaseAdmin';
-import { extrairDiaDaSemana, gerarPresencasParaAluno } from '@/utils/Constants'; // Certifique-se de que este é o caminho correto
-
+import axios from 'axios';
+import { extrairDiaDaSemana, gerarPresencasParaAluno, normalizeName } from '@/utils/Constants';
 const db = admin.database();
 
 export default async function submitForm(req: NextApiRequest, res: NextApiResponse) {
@@ -16,9 +16,9 @@ export default async function submitForm(req: NextApiRequest, res: NextApiRespon
   for (const alunoData of alunos) {
     const { modalidade, turmaSelecionada, aluno } = alunoData;
 
-    // Gerar presenças aqui
-    const diaDaSemana = extrairDiaDaSemana(turmaSelecionada);
-    aluno.presencas = gerarPresencasParaAluno(diaDaSemana);
+     // Gerar presenças aqui
+     const diaDaSemana = extrairDiaDaSemana(turmaSelecionada);
+     aluno.presencas = gerarPresencasParaAluno(diaDaSemana);
 
     if (!turmaSelecionada) {
       resultados.push({ sucesso: false, erro: 'Nome da turma não fornecido', aluno });
@@ -39,12 +39,30 @@ export default async function submitForm(req: NextApiRequest, res: NextApiRespon
       const turma = turmaData[turmaKey];
 
       if (turma.capacidade_atual_da_turma >= turma.capacidade_maxima_da_turma) {
-        resultados.push({ sucesso: false, erro: `Não há vagas disponíveis na turma ${turma.nome_da_turma}, escolha outra turma`, aluno });
+        resultados.push({ sucesso: false, erro: `Não há vagas disponíveis na turma ${turma.nome_da_turma}.`, aluno });
         continue;
       }
 
-      const novoIdAluno = turma.contadorAlunos ? turma.contadorAlunos + 1 : 1;
-      aluno.id = novoIdAluno; // Atribui um ID numérico ao aluno
+      // Verificação de aluno duplicado
+      const alunosSnapshot = await db.ref(`modalidades/${modalidade}/turmas/${turmaKey}/alunos`).once('value');
+    const alunosExistem = alunosSnapshot.val() || {};
+
+    const nomeAlunoNormalizado = normalizeName(aluno.nome);
+    console.log(`Nome normalizado do aluno a ser inserido: ${nomeAlunoNormalizado}`); // Debug
+
+    const alunoDuplicado = Object.values(alunosExistem).some((alunoExistente: any) => {
+        const nomeExistenteNormalizado = normalizeName(alunoExistente.nome);
+        console.log(`Comparando com: ${nomeExistenteNormalizado}`); // Debug
+        return nomeExistenteNormalizado === nomeAlunoNormalizado;
+    });
+
+    if (alunoDuplicado) {
+        resultados.push({ sucesso: false, erro: 'Aluno já cadastrado nesta turma.', aluno });
+        continue;
+    }
+
+      const novoIdAluno = Object.keys(alunosExistem).length + 1;
+      aluno.id = novoIdAluno;
 
       await db.ref(`modalidades/${modalidade}/turmas/${turmaKey}/alunos/${novoIdAluno}`).set(aluno);
       await db.ref(`modalidades/${modalidade}/turmas/${turmaKey}`).update({
@@ -54,10 +72,14 @@ export default async function submitForm(req: NextApiRequest, res: NextApiRespon
 
       resultados.push({ sucesso: true, aluno });
     } catch (erro) {
-      resultados.push({ sucesso: false, erro: erro, aluno });
+      if (axios.isAxiosError(erro)) {
+        // do something
+        // or just re-throw the error
+        resultados.push({ sucesso: false, erro: erro.toString(), aluno });
+      } 
+      
     }
   }
 
   return res.status(200).json({ resultados });
 }
-
