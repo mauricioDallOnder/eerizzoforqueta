eb4cca94157358781d3cdd1a4d20abc31ade3033
eerizzoforqueta <158513004+eerizzoforqueta@users.noreply.github.com>
2024-04-20 19:39:05 -0300

    Update moveStudent.ts

--- src/pages/api/moveStudent.ts
+++ src/pages/api/moveStudent.ts
@@ -4,70 +4,92 @@ import { Aluno } from '@/interface/interfaces';
 
 const db = admin.database();
 
-async function atualizarTurma(modalidade: string, nomeTurma: string, aluno: Aluno | null, incremento: number) {
+async function atualizarTurma(modalidade: string, nomeTurma: string, alunos: Aluno[], incremento: number) {
   const turmaRef = db.ref(`modalidades/${modalidade}/turmas`).orderByChild('nome_da_turma').equalTo(nomeTurma);
   const snapshot = await turmaRef.once('value');
-  if (snapshot.exists()) {
-    const turmaData = snapshot.val();
-    const turmaKey = Object.keys(turmaData)[0];
-    const turma = turmaData[turmaKey];
-
-    if (incremento > 0 && aluno !== null) { // Adiciona aluno
-      const novoIdAluno = turma.contadorAlunos ? turma.contadorAlunos + 1 : 1;
-      aluno.id = novoIdAluno;
-      turma.alunos = turma.alunos || {};
-      turma.alunos[novoIdAluno] = aluno;
-    } // Não precisa de um else aqui, pois não estamos tratando remoção nesta parte
-
-    await db.ref(`modalidades/${modalidade}/turmas/${turmaKey}`).update({
-      alunos: turma.alunos,
-      contadorAlunos: turma.contadorAlunos ? turma.contadorAlunos + incremento : 1,
-      capacidade_atual_da_turma: turma.capacidade_atual_da_turma + incremento
-    });
+  if (!snapshot.exists()) {
+    throw new Error(`Turma ${nomeTurma} não encontrada em modalidade ${modalidade}`);
   }
+
+  const turmaData = snapshot.val();
+  const turmaKey = Object.keys(turmaData)[0];
+  const turma = turmaData[turmaKey];
+
+  // Initialize if undefined
+  turma.alunos = turma.alunos || {};
+  alunos.forEach(aluno => {
+    const novoIdAluno = turma.contadorAlunos ? ++turma.contadorAlunos : 1;
+    aluno.id = novoIdAluno;
+    turma.alunos[novoIdAluno] = aluno;
+  });
+
+  await db.ref(`modalidades/${modalidade}/turmas/${turmaKey}`).update({
+    alunos: turma.alunos,
+    contadorAlunos: turma.contadorAlunos,
+    capacidade_atual_da_turma: turma.capacidade_atual_da_turma + incremento
+  });
 }
 
 async function encontrarEremoverAluno(modalidade: string, nomeDaTurma: string, alunoNome: string): Promise<Aluno | null> {
   const turmaRef = db.ref(`modalidades/${modalidade}/turmas`).orderByChild('nome_da_turma').equalTo(nomeDaTurma);
   const snapshot = await turmaRef.once('value');
-  if (snapshot.exists()) {
-    const turmaData = snapshot.val();
-    const turmaKey = Object.keys(turmaData)[0];
-    const turma = turmaData[turmaKey];
+  if (!snapshot.exists()) {
+    return null;
+  }
 
-    for (const [id, alunoObject] of Object.entries(turma.alunos || {})) {
-      // Assumimos que alunoObject é do tipo Aluno com uma asserção de tipo
-      const aluno = alunoObject as Aluno;
-      if (aluno.nome === alunoNome) {
-        delete turma.alunos[id];
-        await db.ref(`modalidades/${modalidade}/turmas/${turmaKey}/alunos`).set(turma.alunos || {});
-        // Agora que garantimos para o TypeScript que aluno é do tipo Aluno, podemos espalhá-lo sem erro
-        return { ...aluno, id: parseInt(id) };
-      }
+  const turmaData = snapshot.val();
+  const turmaKey = Object.keys(turmaData)[0];
+  const turma = turmaData[turmaKey];
+
+  for (const [id, alunoObject] of Object.entries(turma.alunos || {})) {
+    const aluno = alunoObject as Aluno;
+    if (aluno.nome === alunoNome) {
+      delete turma.alunos[id];
+      await db.ref(`modalidades/${modalidade}/turmas/${turmaKey}/alunos`).set(turma.alunos || {});
+      return { ...aluno, id: parseInt(id) };
     }
   }
   return null;
 }
 
-export default async function moveStudent(req: NextApiRequest, res: NextApiResponse) {
+export default async function moveStudents(req: NextApiRequest, res: NextApiResponse) {
+  console.log("Recebido na API:", req.body);
   if (req.method !== 'POST') {
     res.setHeader('Allow', 'POST');
     return res.status(405).end('Method Not Allowed');
   }
 
-  try {
-    const { alunoNome, modalidadeOrigem, nomeDaTurmaOrigem, modalidadeDestino, nomeDaTurmaDestino } = req.body;
-    const alunoDados = await encontrarEremoverAluno(modalidadeOrigem, nomeDaTurmaOrigem, alunoNome) as Aluno | null;
+  const { alunosNomes, alunosModalidadesOrigem, alunosTurmasOrigem, modalidadeDestino, nomeDaTurmaDestino } = req.body;
 
-    if (alunoDados) {
-      // Agora você pode usar alunoDados com segurança porque o tipo foi asserido para Aluno | null
-      await atualizarTurma(modalidadeDestino, nomeDaTurmaDestino, alunoDados, 1);
-      await atualizarTurma(modalidadeOrigem, nomeDaTurmaOrigem, null, -1);
+  if (!alunosNomes || !alunosModalidadesOrigem || !alunosTurmasOrigem || !modalidadeDestino || !nomeDaTurmaDestino) {
+    const {
+      alunosNomes,
+      alunosModalidadesOrigem,
+      alunosTurmasOrigem,
+      modalidadeDestino,
+      nomeDaTurmaDestino
+    } = req.body;
+    console.log("Recebido na API:", req.body);
+    console.log(alunosNomes, alunosModalidadesOrigem, alunosTurmasOrigem, modalidadeDestino, nomeDaTurmaDestino);
+  }
+
+  try {
+    const alunosRemovidos: Aluno[] = [];
+    for (let i = 0; i < alunosNomes.length; i++) {
+      const alunoRemovido = await encontrarEremoverAluno(alunosModalidadesOrigem[i], alunosTurmasOrigem[i], alunosNomes[i]);
+      if (alunoRemovido) {
+        alunosRemovidos.push(alunoRemovido);
+      }
+    }
+
+    if (alunosRemovidos.length > 0) {
+      await atualizarTurma(modalidadeDestino, nomeDaTurmaDestino, alunosRemovidos, alunosRemovidos.length);
+      return res.status(200).json({ message: 'Alunos movidos com sucesso.' });
     } else {
-      return res.status(404).json({ error: 'Aluno não encontrado na turma de origem' });
+      return res.status(404).json({ error: 'Nenhum dos alunos especificados foi encontrado nas turmas de origem' });
     }
   } catch (error) {
-    console.error('Erro ao mover aluno', error);
-    return res.status(500).json({ error: 'Erro ao mover aluno' });
+    console.error('Erro ao mover alunos', error);
+    return res.status(500).json({ error: 'Erro ao mover alunos' });
   }
 }
