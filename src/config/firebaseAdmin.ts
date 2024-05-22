c57857151162930011b1ff5233046a6b01e978b4
mauricioDallOnder <158513004+eerizzoforqueta@users.noreply.github.com>
2024-05-21 23:17:29 -0300

    fix issues

--- src/config/firebaseAdmin.ts
+++ src/config/firebaseAdmin.ts
@@ -1,11 +1,26 @@
 import * as admin from 'firebase-admin';
+import axios from 'axios';
 
-if (!admin.apps.length) {
-  const serviceAccount = require('./serviceKey.json');
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount), // Substitua serviceAccount pelo seu arquivo de credenciais
-    databaseURL: 'https://bcoescolinha-default-rtdb.firebaseio.com/', // Substitua pela sua URL do banco de dados
-  });
+const serviceAccountUrl = 'https://storage.googleapis.com/bancodedadosceab.appspot.com/serviceKey.json';
+
+async function initializeFirebaseAdmin() {
+  try {
+    const response = await axios.get(serviceAccountUrl);
+    const serviceAccount = response.data;
+
+    if (!admin.apps.length) {
+      admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+        databaseURL: 'https://bcoescolinha-default-rtdb.firebaseio.com/', // Substitua pela sua URL do banco de dados
+      });
+    }
+    
+    console.log('Firebase Admin initialized successfully');
+  } catch (error) {
+    console.error('Error initializing Firebase Admin:', error);
+  }
 }
+
+initializeFirebaseAdmin();
 
 export default admin;
