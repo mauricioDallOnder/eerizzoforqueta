1a17753bae88fd80eedb4fbe050a155d286b910a
eerizzoforqueta <158513004+eerizzoforqueta@users.noreply.github.com>
2024-07-16 08:57:14 -0300

    Update firebaseAdmin.ts

--- src/config/firebaseAdmin.ts
+++ src/config/firebaseAdmin.ts
@@ -5,7 +5,7 @@ if (!admin.apps.length) {
   const serviceAccount = require('./serviceKey.json');
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount), // Substitua serviceAccount pelo seu arquivo de credenciais
-    databaseURL: 'https://escola15072024-default-rtdb.firebaseio.com', // Substitua pela sua URL do banco de dados
+    databaseURL: 'https://bcooficial-dd90c-default-rtdb.europe-west1.firebasedatabase.app', // Substitua pela sua URL do banco de dados
   });
 }
 
